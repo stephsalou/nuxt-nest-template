ff4333b9cc1f853bb2037dbaa58e5e0978d36931
stephane salou <stephane.gambo@smile.ci>
2020-09-05 13:26:00 +0000

    first version template

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'client/components/**/*.vue',
+      'client/layouts/**/*.vue',
+      'client/pages/**/*.vue',
+      'client/plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
