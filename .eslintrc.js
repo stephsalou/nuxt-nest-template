8e66e46346b46df5f851b4911e18de0f4e352d6d
stephane salou <stephane.gambo@smile.ci>
2020-09-05 13:26:16 +0000

    first version template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
-// module.exports = {
-//   root: true,
-//   env: {
-//     browser: true,
-//     node: true,
-//     jest: true,
-//   },
-//   parserOptions: {
-//     project: 'tsconfig.json',
-//     sourceType: 'module',
-//   },
-//   parser: '@typescript-eslint/parser',
-//   extends: [
-//     'plugin:@typescript-eslint/eslint-recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'prettier',
-//     'prettier/@typescript-eslint',
-//     'prettier/vue',
-//     'plugin:prettier/recommended',
-//     'plugin:nuxt/recommended',
-//   ],
-//   plugins: ['prettier', '@typescript-eslint/eslint-plugin'],
-//   // add your custom rules here
-//   rules: {
-//     '@typescript-eslint/interface-name-prefix': 'off',
-//     '@typescript-eslint/explicit-function-return-type': 'off',
-//     '@typescript-eslint/explicit-module-boundary-types': 'off',
-//     '@typescript-eslint/no-explicit-any': 'off',
-//   },
-// }
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint/eslint-plugin'],
+  // add your custom rules here
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
